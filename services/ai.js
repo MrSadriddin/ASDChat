27d1a01d3d5dceefc8e1c430ba39485ea6fac20f
Sadriddin Axhmadullayev <sadriddinaxhmadullayev@MacBook-Pro-Sadriddin.local>
2025-12-07 14:30:50 +0500

    first commit

--- services/ai.js
+++ services/ai.js
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import dotenv from "dotenv";
 
 dotenv.config();
-const GOOGLE_API_KEY = "AIzaSyA4aVNL8AFKzBdZ8nsv2aCrlcz8CX3o7W4"
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
